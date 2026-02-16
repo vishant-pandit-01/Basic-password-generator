397fc869be01874ca6df77be964b7d0fa73eb20e
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-02-16 21:37:01 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+import random
+
+print("Password Generator")
+
+length = int(input("Enter password length: "))
+
+if length <= 0:
+    print("Please enter valid length")
+else:
+    choice = "abcdefghijklmnopqrstuvwxyz"
+    choice += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    choice += "!@#$%^&*_+"
+    choice += "0123456789"
+
+    password = ""
+
+    for i in range(length):
+        password_create = random.choice(choice)
+        password += password_create
+
+    print("Your password is:")
+    print(password)
